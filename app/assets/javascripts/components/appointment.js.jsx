3c635e08ad06e06e03014a9949502f27b5d4adb5
coolJenny <coolJenny009g@gmail.com>
2018-06-01 16:24:27 -0400

    ...

--- app/assets/javascripts/components/appointment.js.jsx
+++ app/assets/javascripts/components/appointment.js.jsx
@@ -3,7 +3,7 @@ var Appointment = createReactClass({
 		return(
 			<div>
 				<h3>{this.props.appointment.title}</h3>
-				<p>{moment(this.props.appointment.appt_time).format('MMMMM DD YYYYY, h:mm:ss a')}</p>
+				<p>{formatDate(this.props.appointment.appt_time)}</p>
 			</div>
 		)
 	}
